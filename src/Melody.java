a05e2a5c223246f19527d36812a706d69dc123e1
greggagne <ggagne@westminsteru.edu>
2023-10-23 16:18:44 -0600

    Initial commit

---
+++ src/Melody.java
@@ -0,0 +1,32 @@
+public class Melody {
+    public Melody(Queue<Note> song) {
+        // TO-DO
+    }
+
+    public double getTotalDuration() {
+        // TO-DO
+        return 0;
+    }
+
+    public void changeTempo(double tempo) {
+        // TO-DO
+    }
+
+    public void reverse() {
+        // TO-DO
+    }
+
+    public void append(Melody other) {
+        // TO-DO
+    }
+
+    public void play() {
+        // TO-DO
+    }
+
+    public String toString() {
+        // TO-DO
+
+        return null;
+    }
+}
